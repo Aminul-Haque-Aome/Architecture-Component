2dafcebf119ee4b53c10c7a8ab9b0cb342033a87
MD. Aminul Haque Aome <aminul15-5281@diu.edu.bd>
2019-04-09 07:37:46 +0600

    Implemeted RxJava

--- app/src/main/java/com/remotearth/fake_coder/architecturecomponent/data/local/GitRepoLocalDataSource.kt
+++ app/src/main/java/com/remotearth/fake_coder/architecturecomponent/data/local/GitRepoLocalDataSource.kt
@@ -1,7 +1,9 @@
 package com.remotearth.fake_coder.architecturecomponent.data.local
 
 import com.remotearth.fake_coder.architecturecomponent.models.Repository
+import io.reactivex.Completable
 import io.reactivex.Observable
+import io.reactivex.Single
 import java.util.concurrent.TimeUnit
 
 class GitRepoLocalDataSource {
@@ -9,23 +11,29 @@ class GitRepoLocalDataSource {
     fun getRepositories(): Observable<ArrayList<Repository>> {
         var repoList = ArrayList<Repository>()
 
-        repoList.add(Repository("FireBase Auth","AomeDIU",100,false))
+        repoList.add(Repository("FireBase Auth", "AomeDIU", 100, false))
         repoList.add(Repository("BloodBond", "receme", 30, true))
-        repoList.add(Repository("Expense Tracer", "Tahmid",430, false))
+        repoList.add(Repository("Expense Tracer", "Tahmid", 430, false))
         repoList.add(Repository("Exertion", "Ashik", 30, false))
         repoList.add(Repository("Prank King", "Amin", 40, false))
-        repoList.add(Repository("Self Protect","Saddam",4,true))
-        repoList.add(Repository("NewsGist","Aslam Hossain",430,false))
-        repoList.add(Repository("BSC Prep Key","Aslam Hossain",430,false))
+        repoList.add(Repository("Self Protect", "Saddam", 4, true))
+        repoList.add(Repository("NewsGist", "Aslam Hossain", 430, false))
+        repoList.add(Repository("BSC Prep Key", "Aslam Hossain", 430, false))
 
         return Observable
             .just(repoList)
             .delay(
                 2,
-                TimeUnit.SECONDS)
+                TimeUnit.SECONDS
+            )
     }
 
-    fun saveRepositories(arrayList: ArrayList<Repository>){
-        //todo save repositories in DB
+    fun saveRepositories(arrayList: ArrayList<Repository>): Completable {
+        return Single
+            .just(1)
+            .delay(
+                1,
+                TimeUnit.SECONDS
+            ).toCompletable()
     }
 }
